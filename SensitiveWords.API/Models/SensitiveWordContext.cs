6f6d79ce740c516b85744304a32bc850d2fa6a61
Adam Chin <adammitchellchin@gmail.com>
2022-10-16 21:40:41 +0200

    initial commit

---
+++ SensitiveWords.API/Models/SensitiveWordContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SensitiveWords.API.Models
+{
+    public class SensitiveWordContext : DbContext
+    {
+        public SensitiveWordContext(DbContextOptions<SensitiveWordContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SensitiveWord> SensitiveWords { get; set; }
+    }
+}
